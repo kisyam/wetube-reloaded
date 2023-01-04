31881d18527f12d7bc95b6dd84050b0707eb245f
kisyam <how2logout@gmail.com>
2023-01-05 00:19:51 +0900

    230105

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,6 @@
+export const join = (req, res) => console.log("Join");
+export const edit = (req, res) => console.log("Edit User");
+export const remove = (req, res) => console.log("Remove User");
+export const login = (req, res) => console.log("Login");
+export const logout = (req, res) => console.log("Logout");
+export const see = (req, res) => console.log("See User");
--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouters";
+import userRouter from "./routers/userRouters";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const home = (req,res) => res.send("home");
-app.get("/", home);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
 
