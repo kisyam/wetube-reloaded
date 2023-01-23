c8f46394be9222d6a1a60357fbc96daafd4c0268
kisyam <how2logout@gmail.com>
2023-01-23 14:32:08 +0900

    edit video part two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,11 +33,11 @@ export const watch = (req, res) => {
   const video = videos[id-1];
   return res.render("watch", {pageTitle:`Watching: ${video.title}`,video});
 };
-export const edit = (req, res) => {
+export const getEdit = (req, res) => {
   const {id} = req.params;
   const video = videos[id-1];
   return res.render("edit", {pageTitle:`Editing: ${video.title}`, video});
 };
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Ready to Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const postEdit = (req,res) => {
+  return res.end();
+};
